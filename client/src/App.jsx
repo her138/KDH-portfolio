dea63098286c4e598828e143fea388bae2de1d76
her138 <94360125+her138@users.noreply.github.com>
2026-04-07 13:23:25 +0200

    Fix import path for Reviews component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Reviews from './components/REviews';
+import Reviews from './components/Reviews';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
